0bf3ff64e2bea1bf9e55af3394df262f2a08c61d
Preetirachel <preetirachel.official@gmail.com>
2023-09-18 10:59:33 +0100

    extended calc

--- IdeaProjects/untitled/src/calculator/calc.java
+++ IdeaProjects/untitled/src/calculator/calc.java
@@ -1,7 +1,8 @@
 package calculator;
 import java.util.Scanner;
 import java.util.Scanner;
-
+import java.io.*;
+import java.lang.Math;
 public class calc {
 
     public static void main(String[] args) {
@@ -9,11 +10,17 @@ public class calc {
 
         Scanner myObj = new Scanner(System.in);
         System.out.println("enter number one");
-        int n1= myObj.nextInt();
+        int n1 = myObj.nextInt();
         System.out.println("enter number two");
-        int n2= myObj.nextInt();
+        int n2 = myObj.nextInt();
         System.out.println("enter which operation you prefer");
-        String operator= myObj.next();
+        String operator = myObj.next();
+
+        System.out.println("the answer is" + calculator(n1, operator, n2));
+    }
+
+        public static int calculator(int n1,String operator,int n2)
+        {
         int a=0;
 
         switch(operator) {
@@ -33,11 +40,22 @@ public class calc {
                 a = n1 / n2;
                 break;
 
+            case "mod":
+                a= n1%n2;
+                break;
+
+            case "exp":
+               double fn=n1;
+                double sn =n2;
+                a = (int)(Math.pow(fn,sn));
+
+                break;
+
             default:
                 System.out.println("enter a valid operator");
                 break;
         }
-        System.out.println("the answer is" + a);
+       return a;
 
 
 
